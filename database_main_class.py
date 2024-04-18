bba8d725186c779c616271f8f982162dd7c7f936
Rana Universe <142967497+RanaUniverse@users.noreply.github.com>
2024-04-18 09:17:30 +0530

    Create database_main_class.py
    This is the script Where i have defines the class for create the table and its column name
    -Rana Universe

---
+++ database_main_class.py
@@ -0,0 +1,49 @@
+
+'''
+# pip install sqlalchemy
+This is the main module where i will defines the class
+-Rana Universe 🍌🍌🍌
+'''
+
+
+from datetime import date, time, datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, Date
+from sqlalchemy.orm import declarative_base
+
+
+
+
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id_ = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+    email = Column(String)
+    date_of_birth = Column(Date)
+
+
+    def __init__(
+        self,
+        name:str = None,
+        age: int = None,
+        gender: str = None,
+        email: str = None,
+        date_of_birth: date =None
+    ):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.email = email
+        self.date_of_birth = date_of_birth
+
+
+    def __repr__(self):
+        return f"<Student(id={self.id},name={self.name}, age={self.age}, gender={self.gender}, email={self.email}, dob={self.date_of_birth})>"
+
+
